f3d84d9f2b9ef3a358ea9dac43469a3affe268c2
Pedro Henrique Franca de Deus <pedrohenriquefrancadedeus@gmail.com>
2021-04-20 16:01:54 -0300

    Config miraclejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { Dashboard } from './components/Dashboard'
+import { useState } from 'react'
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
+import Modal from 'react-modal'
+
+Modal.setAppElement('root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  )
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTRanTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTRanTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   )
